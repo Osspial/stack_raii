e60426faccecbadc71180f2a8f758a7c813fd65d
Osspial <osspial@gmail.com>
2017-08-16 18:57:05 -0400

    Make buffer-backed stacks functional

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,56 @@
 use std::{ptr, mem};
+use std::ops::Deref;
 
-pub struct StackBase<T> {
+pub struct BBStackBase<T> {
     stack: Vec<T>
 }
 
-pub struct StackElement<'a, T: 'a> {
+pub struct BBStackElement<'a, T: 'a> {
     stack: &'a mut Vec<T>,
     el_index: usize
 }
 
-impl<T> StackBase<T> {
+impl<T> BBStackBase<T> {
     #[inline]
-    pub fn new() -> StackBase<T> {
-        StackBase {
+    pub fn new() -> BBStackBase<T> {
+        BBStackBase {
             stack: Vec::new()
         }
     }
 
     #[inline]
-    pub fn with_capacity(cap: usize) -> StackBase<T> {
-        StackBase {
+    pub fn with_capacity(cap: usize) -> BBStackBase<T> {
+        BBStackBase {
             stack: Vec::with_capacity(cap)
+        }
+    }
+
+    #[inline]
+    pub fn push(&mut self, item: T) -> BBStackElement<T> {
+        self.stack.push(item);
+        BBStackElement {
+            stack: &mut self.stack,
+            el_index: 0
         }
     }
 }
 
-impl<'a, T> StackElement<'a, T> {
+impl<'a, T> BBStackElement<'a, T> {
+    #[inline]
+    pub fn push(&mut self, item: T) -> BBStackElement<T> {
+        self.stack.push(item);
+        BBStackElement {
+            stack: &mut self.stack,
+            el_index: self.el_index + 1
+        }
+    }
+
+    #[inline]
     pub fn top(&self) -> &T {
         unsafe{ self.stack.get_unchecked(self.stack.len() - 1) }
     }
 
+    #[inline]
     pub fn top_mut(&mut self) -> &mut T {
         let top_index = self.stack.len() - 1;
         unsafe{ self.stack.get_unchecked_mut(top_index) }
@@ -50,14 +71,20 @@ impl<'a, T> StackElement<'a, T> {
     }
 }
 
-pub trait Stack {
-    type Item;
-
-    fn as_slice(&self) -> &[Self::Item];
-    fn push(&mut self) -> StackElement<Self::Item>;
+impl<'a, T> Deref for BBStackElement<'a, T> {
+    type Target = [T];
+    fn deref(&self) -> &[T] {
+        &*self.stack
+    }
 }
 
-impl<'a, T> Drop for StackElement<'a, T> {
+impl<'a, T> AsRef<[T]> for BBStackElement<'a, T> {
+    fn as_ref(&self) -> &[T] {
+        &*self
+    }
+}
+
+impl<'a, T> Drop for BBStackElement<'a, T> {
     fn drop(&mut self) {
         self.stack.pop();
         debug_assert_eq!(self.el_index, self.stack.len());
